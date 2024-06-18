42c1a220dbf1bf10781823c0abee99422f2111ad
deadpool <2841954631@qq.com>
2024-06-18 23:05:31 +0800

    fix(setup)

--- setup.sh
+++ setup.sh
@@ -5,16 +5,29 @@ if [ ! $1 ]; then
 else
     timing=$1
 fi
+
+if [ ! $2 ]; then
+    swap_time_threashold=1
+else
+    swap_time_threashold=$2
+fi
+
+if [ ! $3 ]; then
+    dedup_mem_threashold=$((1024 * 1024))
+else
+    dedup_mem_threashold=$3
+fi
+
+
 make -j$(nproc)
 echo umounting...
-sudo umount /mnt/pmem
+sudo umount /mnt/pmem0
 echo Removing the old kernel module...
 sudo rmmod nova
 echo Inserting the new kernel module...
-sudo insmod nova.ko measure_timing=$timing
+sudo insmod nova.ko measure_timing=$timing dedup_mem_threashold=$dedup_mem_threashold
 
 sleep 1
 
 echo mounting...
-sudo mount -t NOVA -o init -o data_cow /dev/pmem0 /mnt/pmem
-#sudo mount -t NOVA -o init /dev/pmem0 /mnt/pmem
+sudo mount -t NOVA -o init -o data_cow /dev/pmem0 /mnt/pmem0
